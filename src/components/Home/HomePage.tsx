343963bd66a5adb135581df7001a1d286595e3ae
Eric De Sedas <eric.desedas.w@gmail.com>
2020-03-21 11:02:31 -0500

    Update form example with formik and yup

--- src/components/Home/HomePage.tsx
+++ src/components/Home/HomePage.tsx
@@ -1,92 +1,105 @@
-import React, { useState } from 'react';
+import React from 'react';
 import DashboardPage from '../BasePages/DashboardPage';
-import { Typography, Input } from '@material-ui/core';
-import { useForm } from 'react-hook-form';
-import { TextField, Button, Grid } from '@material-ui/core';
+import { Typography, MenuItem } from '@material-ui/core';
+import { Button, Grid } from '@material-ui/core';
+import { MuiPickersUtilsProvider } from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns';
+import { Formik, Form, Field } from 'formik';
+import { TextField } from 'formik-material-ui';
+import { DatePicker } from 'formik-material-ui-pickers';
+import * as yup from 'yup';
+
+const options = [
+  { key: "option1", val: "Option 1" },
+  { key: "option2", val: "Option 2" },
+  { key: "option3", val: "Option 3" }
+];
+
+const schema = yup.object().shape({
+  randomData: yup.string().required("Random Data is required"),
+  firstName: yup.string().required("First Name is required"),
+  dropdown: yup.string().required("dropdown is required"),
+  date: yup.date().required('Date is required')
+})
 
 const HomePage = () => {
 
-  const [data, updateData] = useState({
-    firstName: 'foo',
-    randomData: 'bar'
-  });
-
   return (
     <DashboardPage>
-      <Typography variant='h3' gutterBottom>Home page. Visit /items for an actual page</Typography>
-      <SubComponent
-        firstName={data.firstName}
-        randomData={data.randomData}
-        parentMethod={data => {
-          updateData(data);
-          console.log(data);
+      <Typography variant='h5'>
+
+      </Typography>
+      <Formik
+        validationSchema={schema}
+        initialValues={{
+          randomData: 'random data',
+          firstName: 'First Name',
+          dropdown: 'option1',
+          date: Date.now()
         }}
-      />
+        onSubmit={(values, {setSubmitting}) => {
+          setSubmitting(false);
+          console.log(values);
+        }}
+      >
+      {({submitForm}) => (
+          <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <Form>
+              <Grid container spacing={3}>
+                <Grid item xs={12}>
+                  <Field
+                    component={TextField}
+                    name='randomData'
+                    label='Random Data'
+                    fullWidth variant='outlined'
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Field
+                    component={TextField}
+                    name='firstName'
+                    label='First Name'
+                    fullWidth variant='outlined'
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Field
+                    component={TextField}
+                    name='dropdown'
+                    label='Drop Down'
+                    select variant='outlined' fullWidth
+                  >
+                    {options.map((option: any) => (
+                      <MenuItem key={option.key} value={option.key}>{option.val}</MenuItem>
+                    ))}
+                  </Field>
+                </Grid>
+                <Grid item xs={12}>
+                  <Field
+                    component={DatePicker}
+                    name='date'
+                    label='Date Picker'
+                    format='MM/dd/yyyy'
+                    fullWidth variant='inline' inputVariant='outlined'
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Button
+                    color='primary'
+                    variant='contained'
+                    fullWidth
+                    onClick={submitForm}
+                  >
+                    Submit
+                  </Button>
+                </Grid>
+              </Grid>
+            </Form>
+          </MuiPickersUtilsProvider>
+        )}
+      </Formik>
     </DashboardPage>
   )
-}
-
-// This logic is only encased in a sub component to test how to pass data to the parent state.
-type SubComponentProps = {
-  firstName: string,
-  randomData: string,
-  parentMethod: (params: any) => void
-}
-
-const SubComponent = (props: SubComponentProps) => {
-
-  const { register, handleSubmit, errors } = useForm();
-
-  const onSubmit = (data: any) => {
-    props.parentMethod(data);
-  }
-
-  return (
-    <form style={{maxWidth: 300}} onSubmit={handleSubmit(onSubmit)}>
-      <Grid container spacing={3}>
-        <Grid item xs={12}>
-          <TextField
-            name='randomData'
-            label='Random Data'
-            inputRef={register({ required: "Input is Required" })}
-            defaultValue={props.randomData}
-            error={typeof errors.randomData !== 'undefined'}
-            helperText={errors.randomData && errors.randomData.message}
-            fullWidth
-            variant='outlined'
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            name='first_name'
-            label='First Name'
-            variant='outlined'
-            fullWidth
-            defaultValue={props.firstName}
-            inputRef={register({ required: "Input is Required", pattern: /^[A-Za-z]+$/ })}
-            error={typeof errors.first_name !== 'undefined'}
-            helperText={
-              errors.first_name &&
-              <span>
-                {errors.first_name.type === 'required' && errors.first_name.message}
-                {errors.first_name.type === 'pattern' && 'Must only use letters, no number allowed'}
-              </span>
-            }
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <Button
-            color='primary'
-            variant='contained'
-            type='submit'
-            fullWidth
-          >
-            Submit
-          </Button>
-        </Grid>
-      </Grid>
-    </form>
-  );
 }
 
 export default HomePage;
